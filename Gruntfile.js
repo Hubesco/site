cd158289944767ec94b2416d531e0b2606771139
Nicolas Barbotte <n.barbotte@gmail.com>
2013-04-15 19:14:53 +0200

    initial commit

---
+++ Gruntfile.js
@@ -0,0 +1,58 @@
+module.exports = function (grunt) {
+
+  'use strict';
+
+  // Project configuration.
+  grunt.initConfig({
+    pkg: grunt.file.readJSON('package.json'),
+    jshint: {
+      files: [
+        'Gruntfile.js'
+      ],
+      options: {
+        browser: true,
+        camelcase: true,
+        curly: true,
+        eqeqeq: true,
+        forin: true,
+        immed: true,
+        indent: 2,
+        latedef: true,
+        newcap: true,
+        noarg: true,
+        noempty: true,
+        quotmark: 'single',
+        undef: true,
+        unused: true,
+        trailing: true,
+        predef: [
+          'module'
+        ]
+      }
+    },
+    less: {
+      production: {
+        files: {
+          'public/css/style.css': 'public/css/style.less'
+        }
+      }
+    },watch: {
+      scripts: {
+        files: ['**/*.less'],
+        tasks: ['less'],
+        options: {
+          nospawn: true
+        }
+      }
+    }
+  });
+
+  // Load tasks.
+  grunt.loadNpmTasks('grunt-contrib-jshint');
+  grunt.loadNpmTasks('grunt-contrib-less');
+  grunt.loadNpmTasks('grunt-contrib-watch');
+
+  // Task definition.
+  grunt.registerTask('default', ['jshint', 'less']);
+
+};
